c37c2ff4f35e5943f176645e2c143d68bb48e3df
anishfrigerio <114395195+anishfrigerio@users.noreply.github.com>
2022-12-14 23:49:20 +0100

    Add files via upload

---
+++ movies/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { AuthGuard } from './auth.guard';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/log-in', pathMatch: 'full' },
+  { path: 'log-in', component: SigninComponent },
+  { path: 'sign-up', component: SignupComponent },
+  { path: 'user-profile/:id', component: UserProfileComponent, canActivate: [AuthGuard] }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ movies/src/app/user.ts
@@ -0,0 +1,6 @@
+export class User {
+  id: number | undefined;
+  name: String | undefined;
+  email: String | undefined;
+  password: String | undefined;
+}
